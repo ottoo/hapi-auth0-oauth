32b84a910d28ad527a9704ba422e512055f016ea
Otto Kivikärki <otto.kivikarki@vincit.fi>
2019-01-06 16:50:23 +0200

    Use BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Redirect, Route, Switch } from 'react-router';
-import { createBrowserHistory } from 'history';
+import { Redirect, Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import Home from './views/Home';
 import PostLogin from './components/PostLogin';
 import './index.css';
 
-const history = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={history}>
+  <BrowserRouter>
     <Switch>
       <Route exact={true} path="/post-login" component={PostLogin} />
       <Route exact={true} path="/" component={App} />
       <Route exact={true} path="/home" component={Home} />
       <Redirect to="/404" />
     </Switch>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
